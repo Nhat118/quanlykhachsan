d70ae426b39833227c392bd0e0837fc81555ad4a
Nhat118 <117331567+Nhat118@users.noreply.github.com>
2024-12-07 19:37:22 +0700

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE KhachHang (
+    ID INT IDENTITY(1,1) PRIMARY KEY, -- ID tự tăng
+    FullName NVARCHAR(255) NOT NULL,  -- Họ và tên
+    Email NVARCHAR(255) UNIQUE NOT NULL, -- Email (phải duy nhất)
+    Password NVARCHAR(MAX) NOT NULL,  -- Mật khẩu đã mã hóa
+    CreatedAt DATETIME DEFAULT GETDATE() -- Thời gian đăng ký
+);
+select * from KhachHang
